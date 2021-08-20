523dd67deed6cf4e149f201c36a4413325d5ef88
RGTS <mroberson115@gmail.com>
2021-08-20 12:29:10 -0300

    Add cors

--- server/server.js
+++ server/server.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.use("/", require("./route/productsRoute"));
 
